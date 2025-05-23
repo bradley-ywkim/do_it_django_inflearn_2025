daa2ade95f2e0d6d8c05250f610abdbbfcf44c92
Brad <connor.ywkim@gmail.com>
2025-05-24 03:19:26 +0900

    모델 생성 및 시간 설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+#라이브러리 > 패키지 > 모듈 > 클래스 > 매서드
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)  #문자 50자까지 받을 수 있음
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #author
+    #만들었으면 cmder에서 makemigrations
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
+
+
+
+
+
