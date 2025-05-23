25fab247df0ef611035c6678bc216405900fbc29
Brad <connor.ywkim@gmail.com>
2025-05-24 03:54:21 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 #라이브러리 > 패키지 > 모듈 > 클래스 > 매서드
 
+#Post 테이블 만들기
 class Post(models.Model):
     title = models.CharField(max_length=50)  #문자 50자까지 받을 수 있음
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
+    created_at = models.DateTimeField(auto_now_add=True) #admin페이지에서 조회 불가
+    updated_at = models.DateTimeField(auto_now=True) #admin페이지에서 조회 불가
     #author
     #만들었으면 cmder에서 makemigrations
 
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import Post
 
+
+#views.py 관리방안
+#FBV: Function Based -> 단순 get, post
+#CBV: Class Based -> 이게 유지보수 get, post관리상좋다.
+
+#views.py <-> models.py 커뮤니케이션
+
 def index(request):
-    posts = Post.objects.all()
+    #posts = Post.objects.all().order_by('-pk') #내림차순
+    posts = Post.objects.all().order_by('pk') #오름차순 
 
 
     return render(
