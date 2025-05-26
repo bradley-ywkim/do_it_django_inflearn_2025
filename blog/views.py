a31a4385e8f19ce1bde2e3be8729db8ba1491de7
Brad <connor.ywkim@gmail.com>
2025-05-26 10:39:54 +0900

    포스트 작성하기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post, Category, Tag
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 # views.py 관리방안
@@ -12,15 +12,20 @@ from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
     model = Post
-    ordering = '-pk'
-
-    # template_name = 'blog/post_list.html' -> 지워도 됨
+    ordering = '-pk'  # 생성일 내림차순 정렬 (최신 글 위로)
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
+
+
+class PostCreate(CreateView):
+    model = Post #레코드를 만들 수 있게 하는 기능
+    #post의 어떤것들을 쓸것인지 정의
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
 
 
 class PostDetail(DetailView):
@@ -37,10 +42,10 @@ class PostDetail(DetailView):
 def category_page(request, slug):
     if slug == 'no_category':
         category = '미분류'
-        post_list = Post.objects.filter(category=None)
+        post_list = Post.objects.filter(category=None).order_by('-pk')  # 최신순 정렬
     else:
         category = Category.objects.get(slug=slug)
-        post_list = Post.objects.filter(category=category)
+        post_list = Post.objects.filter(category=category).order_by('-pk')  # 최신순 정렬
 
     return render(
         request,
@@ -55,16 +60,13 @@ def category_page(request, slug):
 
 
 
+
 def tag_page(request, slug):
-    tag =  Tag.objects.get(slug=slug)
-    post_list = tag.post_set.all()
-    return render(
-        request,
-        'blog/post_list.html',
-        {
-            'post_list': post_list,
-            'categories': Category.objects.all(),
-            'no_category_post_count': Post.objects.filter(category=None).count(),
-             'tag':tag
-        }
-    )
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {
+        'post_list': post_list,
+        'categories': Category.objects.all(),
+        'no_category_post_count': Post.objects.filter(category=None).count(),
+        'tag': tag,
+    })
